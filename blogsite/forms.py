16fed7334d41d754d46b29ae4ed3a7860ee808b8
Naomi sigu <siguneema@gmail.com>
2019-09-23 20:31:19 +0300

    add a form for a mew blog post

--- blogsite/forms.py
+++ blogsite/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blogsite.models import User
 
@@ -29,6 +29,31 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password',
+                        validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')                                                                
     submit = SubmitField('Login')                                                     
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('User name', 
+                            validators=[DataRequired(), Length(min=5, max=15)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])  
+    submit = SubmitField('Update') 
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username is already taken,try another one')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email is already taken,try another one')
+
+
+class PostForm(FlaskForm):
+    title = StringField=('Title', validators=[DataRequired()])
+    content = TextAreaField=('Content', validators=[DataRequired()])
+    submit = SubmitField('create post')
